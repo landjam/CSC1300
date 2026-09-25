e273e4566de88eae8aedb98b40af118fefd73808
landjam <87459429+landjam@users.noreply.github.com>
2026-09-25 00:21:03 -0500

    late night work

--- Labs/Lab4/Lab4.cpp
+++ Labs/Lab4/Lab4.cpp
@@ -1,37 +1,331 @@
 /*
-    Title: Lab4.cpp
+    Title: The College Trail
     Author: Landon Capistrant
     Date: 9/22/2026
     Purpose: College simulator
+    References: 
+        Raw string literals: https://stackoverflow.com/a/5460235
+        cin.get():           https://stackoverflow.com/a/21257728
 */
 
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main() {
     bool endGame = false;
-    char replayResponse;
+    char replayResponse = ' ';
     int day = 1;
-    string name;
+
+    string collegianName;
+    int money;
+    int food = 100;
+    int health = 100;
+    int energy = 100;
+    int stress = 20; // first day jitters
+    int social = 50;
+    int attendance = 5;
+    int study = 5;
+    double gpa = 4.0;
+
+    string const BORDER_BAR = "+====================================================================+";
+    string const EMPTY_BAR  = "|                                                                    |";
+
+    srand(time(0));
+
+    // Give random base moneyz
+    money = (rand() % 5 + 1) * 100;
 
     // Main gameplay loop
-    while (!endGame) {
-        cout << "Please enter your collegian's name: ";
-        cin.ignore();
-        getline(cin, name);
+    while (tolower(replayResponse) != 'n') {
+        // boy, sure wish I could use functions
+        cout << BORDER_BAR << endl;
+        cout << EMPTY_BAR << endl;
+        cout << "|                         THE COLLEGE TRAIL                          |" << endl;
+        cout << EMPTY_BAR << endl;
+        cout << "|             Please resize your console window to fit               |" << endl;
+        cout << "|              the game frame and bottom cursor (>).                 |" << endl;
+        cout << EMPTY_BAR << endl;
+        cout << "|                         Width: 70 characters                       |" << endl;
+        cout << "|                         Height: 25 characters                      |" << endl;
+        cout << EMPTY_BAR << endl;
+        cout << "|                  Press ENTER when you are ready.                   |" << endl;
+        
+        for (int i = 1; i <= 12; i++) {
+            cout << EMPTY_BAR << endl;
+        }
+        
+        cout << BORDER_BAR << endl;
+        cout << ">";
+
+        // Pause until input detected
+        cin.get();
+
+        cout << BORDER_BAR << endl;
+        cout << EMPTY_BAR << endl;
+        cout << EMPTY_BAR << endl;
+        cout << "|                                ---                                 |" << endl;
+        cout << "|                               {^_^}                                |" << endl;
+        cout << "|                              --\\/-  O                              |" << endl;
+        cout << "|                             //|  |\\\\//                             |" << endl;
+        cout << "|                            // |__| --                              |" << endl;
+        cout << "|                            O / |  \\                                |" << endl;
+        cout << "|                              | /\\ |                                |" << endl;
+        cout << "|                              ||  ||                                |" << endl;
+        cout << "|                              O    O                                |" << endl;
+        cout << EMPTY_BAR << endl;
+        cout << EMPTY_BAR << endl;
+        cout << "|                    Please name your collegian:                     |" << endl;
+
+        for (int i = 1; i <= 8; i++) {
+            cout << EMPTY_BAR << endl;
+        }
+
+        cout << BORDER_BAR << endl;
+        cout << ">";
+
+        getline(cin, collegianName);
 
         while (day <= 7) {
-            cout << "Your " << day << " Of school is just beginning.";
+            // Day to string (wish it could be a function instead 😢)
+            string dayString = "";
+            string deathString = "";
+            int response = 0;
+
+            if (day == 1) {
+                dayString = "Monday";
+            } else if (day == 2) {
+                dayString = "Tuesday";
+            } else if (day == 3) {
+                dayString = "Wednesday";
+            } else if (day == 4) {
+                dayString = "Thursday";
+            } else if (day == 5) {
+                dayString = "Friday";
+            } else if (day == 6) {
+                dayString = "Saturday";
+            } else if (day == 7) {
+                dayString = "Sunday";
+            }
+
+            cout << "\n\nIt is " << dayString << "; another great day Of school is just beginning.\n";
+
+            // First random events
+            switch (rand() % 5 + 1) {
+                case 1:
+                    cout << collegianName + R"( wakes up late for school.
+
+1. Get ready normally and accept your tardiness
+2. Run straight to class without getting ready
+3. Go back to sleep and skip class)" << endl;
+                    cin >> response;
+
+                    switch (response) {
+                        case 1:
+                            cout << collegianName + R"( gets to class and everyone stares at them.
+
+Who shows up 45 minutes late to a 50 minute class?
+-gpa +stress -social)";
+
+                            gpa *= 0.85;
+                            stress += 10;
+                            social -= 5;
+                            attendance -= 5;
+                            energy += 10;
+                            health += 10;
+
+                            break;
+                        case 2:
+                            cout << endl << collegianName + " gets to class only a few minutes late and doesn't miss anything.\n\n+gpa +attendance +health";
+
+                            gpa *= 1.1;
+                            social += 5;
+                            attendance += 5;
+                            energy -= 5;
+                            health += 15; // running is good for the heart
+
+                            break;
+                        case 3:
+                            cout << endl << collegianName + " misses the entire lecture from the comfort of their bed.\n\n+gpa +stress -attendance";
+
+                            gpa *= 0.75;
+                            stress += 5;
+                            social -= 10;
+                            attendance -= 10;
+                            energy += 15;
+                            health += 5;
+
+                            break;
+                    }
+
+                    break;
+                case 2:
+                    cout << collegianName + R"( wakes up early before school.
+
+1. Get ready early and study before class starts
+2. Go back to sleep until alarm goes off
+3. Go to the gym and get breakfast before class)" << endl;
+                    cin >> response;
+
+                    switch (response) {
+                        case 1:
+                            cout << collegianName + " studies until it's time for class.\n\n+gpa -stress -energy";
+
+                            gpa *= 1.25;
+                            stress -= 5;
+                            attendance += 5;
+                            energy -= 10;
+                            health -= 5;
+
+                            break;
+                        case 2:
+                            if (rand() % 2 > 0) {
+                                cout << collegianName + " sleeps in until class is about to begin.\n\n +attendance +health";
+
+                                gpa *= 1.1;
+                                attendance += 5;
+                                energy += 5;
+                                health += 5;
+
+                                break;
+                            } else {
+                                cout << collegianName + " sleeps in past the end of class.\n\n -gpa -attendance +energy";
+
+                                gpa *= 0.75;
+                                stress += 5;
+                                social -= 10;
+                                attendance -= 10;
+                                energy += 15;
+                                health += 10;
+
+                                break;
+                            }
+                            
+                        case 3:
+                            cout << collegianName + " pumps some iron and gets daily nutrients prior to class.\n\n+gpa -stress -energy";
+
+                            gpa *= 1.15;
+                            social += 5;
+                            attendance += 5;
+                            energy -= 10;
+                            health += 20;
+                            food += 10;
+
+                            break;
+                    }
+
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+            }
+
+            // Between classes
+            switch (rand() % 3 + 1) {
+                case 1:
+
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+            }
+
+            // Secondary random events
+            switch (rand() % 5 + 1) {
+                case 1:
+
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    
+                    break;
+                case 5:
+                    
+                    break;
+            }
+
+            // Night choice
+            switch (rand() % 3 + 1) {
+                case 1:
+
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+
+                    break;
+            }
+
+            // Stat analysis
+            if (money <= 0) {
+                // Display broke art
+                deathString = " went broke. Can't go to college without money.";
+            } else if (food <= 0) {
+                // Display starvation art
+                deathString = " died of starvation. Food is essential to survival.";
+            } else if (health <= 0) {
+                // Display unhealthy art
+                deathString = " succumbed to bad health. Staying healthy is necessary to college.";
+            } else if (energy <= 0) {
+                // Display lack of energy art
+                deathString = " ran out of energy. Energy is needed in order to do anything.";
+            } else if (stress >= 0) {
+                // Display stressful art
+                deathString = " got stressed out. Don't stress it.";
+            } else if (social <= 0) {
+                // Display lack of social art
+                deathString = " became antisocial. Life is survived together, not alone.";
+            } else if (attendance <= 0) {
+                // Display bad attendance art
+                deathString = " stopped attending. Attendance is required to graduate.";
+            } else if (study <= 0) {
+                // Display lack of studying art
+                deathString = " stopped studying. Can't pass without studying.";
+            } else if (gpa < 2.0) {
+                // Display failed college art
+                deathString = " gpa dropped too far. D's do not get degrees.";
+            }
+
+            if (deathString != "") {
+                cout << collegianName + deathString;
+                break;
+            }
+
+            day++;
         }
+
+        // algorithm to determine performance score
+        cout << endl << endl << "Money: " + to_string(money)
+            + " Food: " + to_string(food)
+            + " Health: " + to_string(health)
+            + " Energy: " + to_string(energy)
+            + " Stress: " + to_string(stress)
+            + " Social: " + to_string(social)
+            + " Attendance: " + to_string(attendance)
+            + " Study: " + to_string(study)
+            + " GPA: " + to_string(gpa);
 
         // End game
         cout << "Thanks for playing! Play again? (y/n): ";
         cin >> replayResponse;
-
-        // Just in case they have caps lock on
-        if (tolower(replayResponse) != 'y') {
-            return 0;
-        }
     }
+
+    return 0;
 }
--- Programs/Program1/ljcapistra42_prog1.cpp
+++ Programs/Program1/ljcapistra42_prog1.cpp
@@ -13,7 +13,219 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, World!" << endl;
+    char replayResponse, answer;
+    int score;
+    string const bar = "--------------------------------------------------";
+
+    while (tolower(replayResponse) != 'n') {
+        // Program init
+        score = 0;
+        cout << endl << bar << endl;
+        cout << "Welcome to Landon's Geography Trivia Game" << endl;
+        cout << bar << endl << endl;
+        cout << "Match the following landmarks to their location in the world to get a point." << endl << endl << endl;
+
+
+        // Question 1
+        cout << "**1**" << endl;
+        cout << "What country is Christ the Redeemer located in?" << endl;
+        cout << "\ta. Brazil" << endl <<
+            "\tb. Peru" << endl <<
+            "\tc. Argentina" << endl <<
+            "\td. Uruguay" << endl << 
+            endl;
+        cout << "ANSWER a, b, c, or d: ";
+        cin >> answer;
+
+        if (answer == 'a') {
+            score += 1;
+            cout << "Correct! one point!" << endl;
+        } else {
+            cout << "Incorrect. better luck next time." << endl;
+        }
+
+        // Question 2
+        cout << endl << "**2**" << endl;
+        cout << "What is the capital of Canada?" << endl;
+        cout << "\ta. Toronto" << endl <<
+            "\tb. Vancouver" << endl <<
+            "\tc. Ottawa" << endl <<
+            "\td. Montreal" << endl <<
+            endl;
+        cout << "ANSWER a, b, c, or d: ";
+        cin >> answer;
+
+        if (answer == 'c') {
+            score += 1;
+            cout << "Correct! one point!" << endl;
+        } else {
+            cout << "Incorrect. better luck next time." << endl;
+        }
+
+        // Question 3
+        cout << endl << "**3**" << endl;
+        cout << "Which country has the largest population in the world?" << endl;
+        cout << "\ta. India" << endl <<
+            "\tb. China" << endl <<
+            "\tc. United States" << endl <<
+            "\td. Indonesia" << endl <<
+            endl;
+        cout << "ANSWER a, b, c, or d: ";
+        cin >> answer;
+
+        if (answer == 'a') {
+            score += 1;
+            cout << "Correct! one point!" << endl;
+        } else {
+            cout << "Incorrect. better luck next time." << endl;
+        }
+
+        // Question 4
+        cout << endl << "**4**" << endl;
+        cout << "Which is the largest continent by area?" << endl;
+        cout << "\ta. Africa" << endl <<
+            "\tb. Europe" << endl <<
+            "\tc. Asia" << endl <<
+            "\td. North America" << endl <<
+            endl;
+        cout << "ANSWER a, b, c, or d: ";
+        cin >> answer;
+
+        if (answer == 'c') {
+            score += 1;
+            cout << "Correct! one point!" << endl;
+        } else {
+            cout << "Incorrect. better luck next time." << endl;
+        }
+
+        // Question 5
+        cout << endl << "**5**" << endl;
+        cout << "Which country is home to the Great Barrier Reef?" << endl;
+        cout << "\ta. Australia" << endl <<
+            "\tb. South Africa" << endl <<
+            "\tc. New Zealand" << endl <<
+            "\td. Indonesia" << endl <<
+            endl;
+        cout << "ANSWER a, b, c, or d: ";
+        cin >> answer;
+
+        if (answer == 'a') {
+            score += 1;
+            cout << "Correct! one point!" << endl;
+        } else {
+            cout << "Incorrect. better luck next time." << endl;
+        }
+
+        // Question 6
+        cout << endl << "**6**" << endl;
+        cout << "Which river is the longest river in South America?" << endl;
+        cout << "\ta. Amazon River" << endl <<
+            "\tb. Mississippi River" << endl <<
+            "\tc. Nile River" << endl <<
+            "\td. Parana River" << endl <<
+            endl;
+        cout << "ANSWER a, b, c, or d: ";
+        cin >> answer;
+
+        if (answer == 'a') {
+            score += 1;
+            cout << "Correct! one point!" << endl;
+        } else {
+            cout << "Incorrect. better luck next time." << endl;
+        }
+
+        // Question 7
+        cout << endl << "**7**" << endl;
+        cout << "Which country is shaped like a boot?" << endl;
+        cout << "\ta. Spain" << endl <<
+            "\tb. Italy" << endl <<
+            "\tc. Greece" << endl <<
+            "\td. Portugal" << endl <<
+            endl;
+        cout << "ANSWER a, b, c, or d: ";
+        cin >> answer;
+
+        if (answer == 'b') {
+            score += 1;
+            cout << "Correct! one point!" << endl;
+        } else {
+            cout << "Incorrect. better luck next time." << endl;
+        }
+
+        // Question 8
+        cout << endl << "**8**" << endl;
+        cout << "Which desert is the largest hot desert in the world?" << endl;
+        cout << "\ta. Gobi Desert" << endl <<
+            "\tb. Arabian Desert" << endl <<
+            "\tc. Sahara Desert" << endl <<
+            "\td. Kalahari Desert" << endl <<
+            endl;
+        cout << "ANSWER a, b, c, or d: ";
+        cin >> answer;
+
+        if (answer == 'c') {
+            score += 1;
+            cout << "Correct! one point!" << endl;
+        } else {
+            cout << "Incorrect. better luck next time." << endl;
+        }
+
+        // Question 9
+        cout << endl << "**9**" << endl;
+        cout << "Which country has the city of Cairo as its capital?" << endl;
+        cout << "\ta. Egypt" << endl <<
+            "\tb. Jordan" << endl <<
+            "\tc. Morocco" << endl <<
+            "\td. Turkey" << endl <<
+            endl;
+        cout << "ANSWER a, b, c, or d: ";
+        cin >> answer;
+
+        if (answer == 'a') {
+            score += 1;
+            cout << "Correct! one point!" << endl;
+        } else {
+            cout << "Incorrect. better luck next time." << endl;
+        }
+
+        // Question 10
+        cout << endl << "**10**" << endl;
+        cout << "Which ocean is located between Africa and Australia?" << endl;
+        cout << "\ta. Atlantic Ocean" << endl <<
+            "\tb. Pacific Ocean" << endl <<
+            "\tc. Arctic Ocean" << endl <<
+            "\td. Indian Ocean" << endl <<
+            endl;
+        cout << "ANSWER a, b, c, or d: ";
+        cin >> answer;
+
+        if (answer == 'd') {
+            score += 1;
+            cout << "Correct! one point!" << endl;
+        } else {
+            cout << "Incorrect. better luck next time." << endl;
+        }
+
+        // Program final
+        // Centering string reference: https://stackoverflow.com/a/17513466
+        cout << endl << endl << string((bar.length() - 7) / 2, ' ') + "RESULTS" << endl;
+        cout << bar << endl;
+        cout << "You scored " << score << " points. ";
+
+        if (score == 10) {
+            cout << "You got all of them right! Incredible job!" << endl;
+        } else if (score > 4) {
+            // no need to check if it's less than 10 since it would've went with the first if
+            cout << "Great Job!" << endl;
+        } else {
+            cout << "You can do better..." << endl;
+        }
+
+        cout << bar << endl << endl;
+
+        cout << "Do you want to play again? (y/n)  ";
+        cin >> replayResponse;
+    }
 
     return 0;
 }
